23484917ca262a9a6688795144de405574321ed4
d-mittal <62880643+d-mittal@users.noreply.github.com>
2025-06-20 14:28:07 +0000

    Deploying to gh-pages from @ d-mittal/d-mittal.github.io@f03e081e8abe50d59b708cf8c78039b9cc92de93 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "Projects",
-          description: "Under construction.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
         },{id: "nav-repositories",
           title: "Repositories",
           description: "",
